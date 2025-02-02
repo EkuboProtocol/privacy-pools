5323c41b31b99bf61b0654c78b40832446411daf
Neo <neotheprogramist@proton.me>
2025-02-03 00:07:05 +0100

    exec proof verification working

--- client/src/tests/pool_account_test.rs
+++ client/src/tests/pool_account_test.rs
@@ -3,8 +3,8 @@ use std::{fmt::Debug, str::FromStr};
 use crate::{
     circuit::{CircuitInputCreator, Commitment},
     deploy_declare::declare::{
-        ERC20_CASM_STR, ERC20_SIERRA_STR, POOL_CASM_STR, POOL_SIERRA_STR, UNIVERSAL_ECIP_CASM_STR,
-        UNIVERSAL_ECIP_SIERRA_STR, VERIFIER_CASM_STR, VERIFIER_SIERRA_STR,
+        ERC20_CASM_STR, ERC20_SIERRA_STR, UNIVERSAL_ECIP_CASM_STR, UNIVERSAL_ECIP_SIERRA_STR,
+        VERIFIER_CASM_STR, VERIFIER_SIERRA_STR,
     },
     prover::Prover,
 };
@@ -17,18 +17,26 @@ use openrpc_testgen::utils::{
         accounts::{
             account::{Account, ConnectedAccount},
             call::Call,
+            creation::helpers::get_chain_id,
+            single_owner::{ExecutionEncoding, SingleOwnerAccount},
         },
         contract::{factory::ContractFactory, CompiledClass, HashAndFlatten, SierraClass},
         endpoints::utils::{get_selector_from_name, wait_for_sent_transaction},
+        providers::jsonrpc::{HttpTransport, JsonRpcClient},
+        signers::{key_pair::SigningKey, local_wallet::LocalWallet},
     },
 };
 use starknet::providers::Url;
 use starknet_crypto::Felt;
+use starknet_types_rpc::{BlockId, BlockTag};
 
 pub const POOL_ACCOUNT_SIERRA_STR: &str =
     include_str!("../../../target/dev/pool_PoolAccount.contract_class.json");
 pub const POOL_ACCOUNT_CASM_STR: &str =
     include_str!("../../../target/dev/pool_PoolAccount.compiled_contract_class.json");
+
+const STRK: Felt =
+    Felt::from_hex_unchecked("0x4718F5A0FC34CC1AF16A1CDEE98FFB20C31F5CD61D6AB07201858F4287C938D");
 
 #[tokio::test]
 async fn test_pool_account_simple() {
@@ -39,17 +47,26 @@ async fn test_pool_account_simple() {
     let private_key = Felt::from_hex_unchecked(
         "0x0000000000000000000000000000000071d7bb07b9a64f6f78ac4c816aff4da9",
     );
+    let class_hash = Felt::from_hex_unchecked(
+        "0x061dac032f228abef9c6626f995015233097ae253a7f72d68552db02f2971b8f",
+    );
+
+    let provider = JsonRpcClient::new(HttpTransport::new(node_url.clone()));
+    let chain_id = get_chain_id(&provider).await.unwrap();
+    let paymaster_private_key = SigningKey::from_secret_scalar(private_key);
+    let mut paymaster_account = SingleOwnerAccount::new(
+        provider.clone(),
+        LocalWallet::from(paymaster_private_key),
+        address,
+        chain_id,
+        ExecutionEncoding::New,
+    );
+    paymaster_account.set_block_id(BlockId::Tag(BlockTag::Pending));
 
     // StarknetHive initialization
-    let hive = StarknetHive::new_with_custom_account(
-        node_url,
-        address,
-        private_key,
-        POOL_ACCOUNT_SIERRA_STR.to_string(),
-        POOL_ACCOUNT_CASM_STR.to_string(),
-    )
-    .await
-    .unwrap();
+    let hive = StarknetHive::new(node_url, address, private_key, class_hash)
+        .await
+        .unwrap();
 
     let erc20_class_hash = declare(&hive, ERC20_SIERRA_STR, ERC20_CASM_STR).await;
     dbg!(erc20_class_hash);
@@ -78,9 +95,10 @@ async fn test_pool_account_simple() {
     let ecip_class_hash = declare(&hive, UNIVERSAL_ECIP_SIERRA_STR, UNIVERSAL_ECIP_CASM_STR).await;
     dbg!(ecip_class_hash);
     let verifier_class_hash = declare(&hive, VERIFIER_SIERRA_STR, VERIFIER_CASM_STR).await;
+    dbg!(verifier_class_hash);
     let verifier_address = deploy(&hive, verifier_class_hash, vec![]).await;
     dbg!(verifier_address);
-    let pool_class_hash = declare(&hive, POOL_SIERRA_STR, POOL_CASM_STR).await;
+    let pool_class_hash = declare(&hive, POOL_ACCOUNT_SIERRA_STR, POOL_ACCOUNT_CASM_STR).await;
     let pool_address = deploy(
         &hive,
         pool_class_hash,
@@ -94,6 +112,20 @@ async fn test_pool_account_simple() {
     )
     .await;
     dbg!(pool_address);
+
+    let transfer_amount = Felt::from_hex("0xfffffffffffffff").unwrap();
+    let transfer_execution = paymaster_account
+        .execute_v3(vec![Call {
+            to: STRK,
+            selector: get_selector_from_name("transfer").unwrap(),
+            calldata: vec![pool_address, transfer_amount, Felt::ZERO],
+        }])
+        .send()
+        .await
+        .unwrap();
+    wait_for_sent_transaction(transfer_execution.transaction_hash, &paymaster_account)
+        .await
+        .unwrap();
 
     let my_commitment = Commitment::new(12345u32, 54321u32, 100u32);
 
@@ -271,34 +303,49 @@ async fn withdraw(
         2u32,
     );
 
+    let node_url = Url::parse("http://localhost:5050").unwrap();
+    let private_key = Felt::from_hex_unchecked(
+        "0x0000000000000000000000000000000071d7bb07b9a64f6f78ac4c816aff4da9",
+    );
+
+    let provider = JsonRpcClient::new(HttpTransport::new(node_url.clone()));
+    let chain_id = get_chain_id(&provider).await.unwrap();
+    let private_key = SigningKey::from_secret_scalar(private_key);
+
+    let mut pool_account = SingleOwnerAccount::new(
+        provider.clone(),
+        LocalWallet::from(private_key),
+        pool_address,
+        chain_id,
+        ExecutionEncoding::New,
+    );
+    pool_account.set_block_id(BlockId::Tag(BlockTag::Pending));
+
     let proof = Prover::new("../target")
         .get_calldata(input_creator.create())
         .await;
 
-    let withdraw_call = Call {
-        to: pool_address,
-        selector: starknet::macros::selector!("withdraw"),
-        calldata: Vec::<starknet::core::types::Felt>::cairo_serialize(&proof),
+    let mut calldata = vec![Felt::from(proof.len())];
+    calldata.extend(proof);
+    let invoke_call = Call {
+        to: STRK,
+        selector: starknet::macros::selector!("transfer"),
+        calldata: calldata,
     };
-    let prepared_execution_v3 = hive
-        .execute_v3(vec![withdraw_call])
+    let prepared_execution_v3 = pool_account
+        .execute_v3(vec![invoke_call])
         .prepare()
         .await
         .unwrap();
 
-    let signature = hive
-        .sign_execution_v3(prepared_execution_v3.get_raw_execution().await, false)
-        .await
-        .unwrap();
-
     let invoke_result_custom_signature = prepared_execution_v3
-        .send_with_custom_signature(signature)
+        .send_with_custom_signature(Default::default())
         .await
         .unwrap();
 
     let tx = wait_for_sent_transaction(
         invoke_result_custom_signature.transaction_hash,
-        &hive.account,
+        &pool_account,
     )
     .await
     .unwrap();
--- pool/src/account.cairo
+++ pool/src/account.cairo
@@ -1,2 +1,1 @@
-pub mod account_mixin;
 pub mod pool_account;
